2967494a34638d03ebd0d89e351b08a215c7fead
y-mabuchi <mabu.you@gmail.com>
2022-10-22 05:22:15 +0900

    add error test in task create usecase

--- usecase/task_test.go
+++ usecase/task_test.go
@@ -87,6 +87,30 @@ func TestTaskUseCase_CreateTask(t1 *testing.T) {
 			want:    success,
 			wantErr: false,
 		},
+		{
+			name: "name is empty",
+			args: args{
+				name:    "",
+				dueDate: dueDate,
+			},
+			prepare: func(m *mock_infra.MockTaskRepositoryInterface) {
+				m.EXPECT().Save(gomock.Any()).AnyTimes()
+			},
+			want:    nil,
+			wantErr: true,
+		},
+		{
+			name: "due date is empty",
+			args: args{
+				name:    "task test",
+				dueDate: time.Time{},
+			},
+			prepare: func(m *mock_infra.MockTaskRepositoryInterface) {
+				m.EXPECT().Save(gomock.Any()).AnyTimes()
+			},
+			want:    nil,
+			wantErr: true,
+		},
 	}
 
 	for _, tt := range tests {
@@ -109,6 +133,68 @@ func TestTaskUseCase_CreateTask(t1 *testing.T) {
 			}
 			if !reflect.DeepEqual(got, tt.want) {
 				t1.Errorf("CreateTask() got = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestTaskUseCase_PostponeTask(t1 *testing.T) {
+	dueDate := time.Date(2020, 1, 1, 0, 0, 0, 0, time.UTC)
+	success := &domain.Task{
+		ID:            0,
+		TaskStatus:    domain.UnDone,
+		Name:          "task test",
+		DueDate:       dueDate,
+		PostponeCount: 0,
+	}
+	postponed := &domain.Task{
+		ID:            0,
+		TaskStatus:    domain.UnDone,
+		Name:          "task test",
+		DueDate:       dueDate.AddDate(0, 0, 1),
+		PostponeCount: 1,
+	}
+
+	type fields struct {
+		repo mock_infra.MockTaskRepositoryInterface
+	}
+	type args struct {
+		id int
+	}
+	tests := []struct {
+		name    string
+		fields  fields
+		args    args
+		prepare func(m *mock_infra.MockTaskRepositoryInterface)
+		wantErr bool
+	}{
+		{
+			name: "success",
+			args: args{
+				id: 1,
+			},
+			prepare: func(m *mock_infra.MockTaskRepositoryInterface) {
+				m.EXPECT().FindById(1).Return(success, nil)
+				m.EXPECT().Save(success).Return(postponed, nil)
+			},
+			wantErr: false,
+		},
+	}
+	for _, tt := range tests {
+		t1.Run(tt.name, func(t1 *testing.T) {
+			ctrl := gomock.NewController(t1)
+			defer ctrl.Finish()
+
+			mock := mock_infra.NewMockTaskRepositoryInterface(ctrl)
+
+			tt.prepare(mock)
+
+			t := &TaskUseCase{
+				repo: mock,
+			}
+
+			if err := t.PostponeTask(tt.args.id); (err != nil) != tt.wantErr {
+				t1.Errorf("PostponeTask() error = %v, wantErr %v", err, tt.wantErr)
 			}
 		})
 	}
